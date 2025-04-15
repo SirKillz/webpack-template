a97acb36c686666f3056121ae819d5c4106b09ff
Nick Killeen <nick@aleaquo.com>
2025-04-15 08:17:01 -0500

    Configured eslint with airbnb linting

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// .eslintrc.js
+module.exports = {
+    env: {
+      browser: true,
+      node: true,
+      es2021: true,
+    },
+    extends: ['airbnb-base'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      // Add any overrides here
+    },
+  };
+  
